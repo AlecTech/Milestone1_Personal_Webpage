3da9f3145b2648a8c4c009e0365400113c6fe742
AlecTech <eremeevoleg@gmail.com>
2020-09-04 11:37:32 -0600

    Start JS contact form

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,94 @@ function show() {
     } 
 }
 
+const validForm = document.querySelector('#validForm');
+console.log(validForm);
+const fName = document.querySelector('#fullName');
+console.log(fName);
+const subject = document.querySelector('#subj');
+console.log(subject);
+const uEmail = document.querySelector('#userEmail');
+console.log(uEmail);
+const uText = document.querySelector('#userText');
+console.log(uText);
+const clickSubmit = document.querySelector('input[type="submit"]');
+console.log(clickSubmit);
+var error_message = document.getElementById("error_message");
+console.log(error_message);
+
+
+
+//  validForm.addEventListener('submit', (event) => 
+//  {
+//         event.preventDefault();
+//          = "visible";
+//         if (i<0) {
+//             let x = .value;
+//             if () {
+                
+//             }
+//             .value = "";
+//         }
+//         else if () {
+//             ;
+//         }
+
+//         if () {
+            
+//         }
+
+//         if () {
+            
+//         }
+
+//         console.log();
+// });
+//     /* Reset & Play Again form event listener */
+//     playAgainForm.addEventListener('submit', (event) => 
+//     {
+//         event.preventDefault();
+//         if (chancesCntr > 7 || winner == true) {
+//             init();
+//         }
+//         else {
+//             if () {
+//                 init();
+//             }
+//         }
+//     });
+
+
+
+// form.addEventListener('submit', (event) => 
+//     {   //check every stap at the time to make sure that input data is still there
+//         event.preventDefault();
+//         console.log(event);
+//         // method FormData found on website 
+//         //https://javascript.info/formdata#:~:text=let%20formData%20%3D%20new%20FormData(%5B,%3A%20multipart%2Fform%2Ddata%20.
+//         let newPost = new FormData (form);
+//         console.log(newPost.get('title'));
+//         console.log(newPost.get('content'));
+
+//         let a = newPost.get('title');
+//         console.log(a);
+//         let b = newPost.get('content');
+//         console.log(b);
+//             // check if input is not empty                                            
+//         if ( a.length > 0 && b.length > 0)           
+//         {      
+//             // print out values that are entered by the user at the bottom of the page
+//             let data = new Article (a,b);
+//             data.output();
+//             const time = new Date().toLocaleString('en-US', { timeZone: 'UTC' })      
+//             console.log(time);                  
+//         }
+//         else
+//         {
+//             alert("Warrning this is am empty input!");
+//         }    
+//     })
+//     // print out articles at refresh that were collcted by (for of - loop) on line 83
+// for (const article of articles)
+// {
+//     article.output();
+// }
